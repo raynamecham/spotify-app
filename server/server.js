182aadbc305120ca927a559d69cdf0dd5a998591
raynamecham <rayna.mecham@gmail.com>
2021-08-24 18:05:20 -0600

    setting up dashboard, api requests

--- server/server.js
+++ server/server.js
@@ -4,9 +4,26 @@ const SpotifyWebApi = require("spotify-web-api-node");
 const app = express();
 
 app.post("/login", (req, res) => {
+  const code = req.body.code;
+
   const spotifyApi = new SpotifyWebApi({
     redirectUri: "http://localhost:3000/callback",
     clientId: "eb8b221a8f4d4ad2ab315d9dd2f66ffd",
     clientSecret: "7e546191f2d248b184bc5785d9c241cf",
   });
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch(() => {
+      res.sendStatus(400);
+    });
 });
+
+app.listen(3001);
