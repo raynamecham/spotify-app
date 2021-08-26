519af05940c896da13bfbc0c7ae16fa792517efd
raynamecham <rayna.mecham@gmail.com>
2021-08-26 14:36:34 -0600

    working on useAuth

--- server/server.js
+++ server/server.js
@@ -1,7 +1,34 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
 const SpotifyWebApi = require("spotify-web-api-node");
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:3000/callback",
+    clientId: "eb8b221a8f4d4ad2ab315d9dd2f66ffd",
+    clientSecret: "7e546191f2d248b184bc5785d9c241cf",
+    refreshToken,
+  });
+
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      res.json({
+        accessToken: data.body.accessToken,
+        expiresIn: data.body.expiresIn,
+      });
+    })
+    .catch(() => {
+      res.sendStatus(400);
+    });
+});
 
 app.post("/login", (req, res) => {
   const code = req.body.code;
@@ -21,7 +48,8 @@ app.post("/login", (req, res) => {
         expiresIn: data.body.expires_in,
       });
     })
-    .catch(() => {
+    .catch((err) => {
+      console.log(err);
       res.sendStatus(400);
     });
 });
